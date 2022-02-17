f5dfcd715de3c4253941698c5e86f3063ed03652
Antipixel <tyler.w.2008@gmail.com>
2022-02-17 18:23:01 +0800

    Added example Wi-Fi credentials file

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,57 @@
 #include <AsyncUDP.h>
 
 #include "rx8cluster.h"
-#include "credentials.h"
+#include "wificredentials.h"
 #include "forza/forzadata.h"
 
 #define SERIAL_BAUD 115200
-#define MESSAGE_FREQUENCY 15
 #define DATA_OUT_PORT 8855
+#define MESSAGE_FREQUENCY 15
 #define WIFI_CONNECT_TIMEOUT 15000
 
 RX8Cluster cluster(GPIO_NUM_5, GPIO_NUM_4);
 CAN_device_t CAN_cfg;
 AsyncUDP udp;
+
+/**
+ * 
+ * @param packet the UDP packet containing the Forza telemetry data
+ */
+void onDatagramReceived(AsyncUDPPacket packet)
+{
+    ForzaData *data = (ForzaData*)packet.data();
+
+    // Check the gameplay state. This flag is set while driving,
+    // if game is paused, in a menu, etc. this flag will not be set
+    // and the data contained in this packet will be empty/invalid 
+    if (!data->enabled)
+        return;
+
+    // Set RPM and vehicle speeds. The speedometer maxes out at 299 km/h,
+    // any attempt to display a speed over in this results in the display
+    // reverting back to zero. The tachometer maxes out at about 11.2k RPM,
+    // conveniently just as it begins to obscure the speedometer readout
+    cluster.setEngineSpeed(data->currentRPM);
+    cluster.setVehicleSpeed(data->speed);
+
+    // Set the gear display. Only values possible from Forza are reverse
+    // or the numeric gear value. The RX8 cluster is only capable of displaying
+    // numeric gear values up to 6, any specified value over this will cap at 6
+    cluster.setGearPosition(Transmission::getGearForPosition(data->gear));
+
+    // Though Forza doesn't provide data for the state of ABS, we can make
+    // a surprisingly accurate estimate using the speed of the vehicle, the 
+    // position of the brake pedal and the speed of each of the wheels
+    bool vehicleMoving = data->speed > 1.0;
+    bool brakingHard = data->brakePedalPosition > 220;
+    bool wheelsLocked = data->wheelRotationSpeed.getTotal() < 1.0;
+
+    // If the vehicle is still in motion with the brake pedal to the floor; yet the wheel
+    // speed isn't at or very near to zero, we can take a guess at this meaning the ABS 
+    // is active and doing its job of preventing the brakes from locking up completely
+    cluster.setAntilockLight(vehicleMoving && brakingHard && !wheelsLocked);
+    cluster.setHandbrakeLight(data->handBrake);
+}
 
 uint8_t attemptWiFiConnect(const char *ssid, const char *pass)
 {
@@ -56,46 +96,6 @@ void initialiseNetwork()
     if (udp.listen(DATA_OUT_PORT)) {
         udp.onPacket(onDatagramReceived);
     }
-}
-
-/**
- * 
- * @param packet the UDP packet containing the Forza telemetry data
- */
-void onDatagramReceived(AsyncUDPPacket packet)
-{
-    ForzaData *data = (ForzaData*)packet.data();
-
-    // Check the gameplay state. This flag is set while driving,
-    // if game is paused, in a menu, etc. this flag will not be set
-    // and the data contained in this packet will be empty/invalid 
-    if (!data->enabled)
-        return;
-
-    // Set RPM and vehicle speeds. The speedometer maxes out at 299 km/h,
-    // any attempt to display a speed over in this results in the display
-    // reverting back to zero. The tachometer maxes out at about 11.2k RPM,
-    // conveniently just as it begins to obscure the speedometer readout
-    cluster.setEngineSpeed(data->currentRPM);
-    cluster.setVehicleSpeed(data->speed);
-
-    // Set the gear display. Only values possible from Forza are reverse
-    // or the numeric gear value. The RX8 cluster is only capable of displaying
-    // numeric gear values up to 6, any specified value over this will cap at 6
-    cluster.setGearPosition(Transmission::getGearForPosition(data->gear));
-
-    // Though Forza doesn't provide data for the state of ABS, we can make
-    // a surprisingly accurate estimate using the speed of the vehicle, the 
-    // position of the brake pedal and the speed of each of the wheels
-    bool vehicleMoving = data->speed > 1.0;
-    bool brakingHard = data->brakePedalPosition > 220;
-    bool wheelsLocked = data->wheelRotationSpeed.getTotal() < 1.0;
-
-    // If the vehicle is still in motion with the brake pedal to the floor; yet the wheel
-    // speed isn't at or very near to zero, we can take a guess at this meaning the ABS 
-    // is active and doing its job of preventing the brakes from locking up completely
-    cluster.setAntilockLight(vehicleMoving && brakingHard && !wheelsLocked);
-    cluster.setHandbrakeLight(data->handBrake);
 }
 
 void setup() 
---
+++ src/wificredentials.h.example
@@ -0,0 +1,2 @@
+const char *WIFI_SSID = "<SSID>";
+const char *WIFI_PASS = "<PASSWORD>";
